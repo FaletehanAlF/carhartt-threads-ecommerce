87ac49352558d24708c038eec2fa706292af7b0f
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-21 18:49:39 +0700

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app_router.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     const ProviderScope(
       child: CarharttApp(),
